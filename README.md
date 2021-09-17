233d6650bc7e036a9d0d57f3ac838791dee606e7
Stevan <83223136+0xEvke@users.noreply.github.com>
2021-09-18 00:05:20 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+"# evke.github.io" 
---
+++ src/content_option.js
@@ -0,0 +1,98 @@
+const logotext = "Evke";
+const meta = {
+    title: "Evke",
+    description: "I’m Evke Full stack web developer",
+};
+
+const introdata = {
+    title: "I’m Evke",
+    animated: {
+        first: "I love C",
+        second: "I code Web Apps",
+        third: "I secure Web Apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "I'm 14 years old Web Developer and Web App Pentester from Serbia, I'm currently learning C",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+
+const skills = [{
+        name: "C",
+        value: 30,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Django",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Express.js",
+        value: 95,
+    }
+];
+
+const services = [{
+        title: "C",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Web Development",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Web Security",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "/assets/images/social-media_resize_md",
+        desctiption: "Api for Socail Media.",
+        link: "https://github.com/0xEvke/api-for-socailmedia",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "evkejedanjedini@gmail.com",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/0xEvke",
+    facebook: "https://facebook.com/0xEvke",
+    linkedin: "https://linkedin.com/0xEvke",
+    twitter: "https://twitter.com/0xEvke",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
